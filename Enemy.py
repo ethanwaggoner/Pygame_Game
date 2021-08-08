71a5cd0bec96f07b2a65f613de980677b4ec677c
ethanwaggoner <74027222+ethanwaggoner@users.noreply.github.com>
2021-08-08 19:26:36 -0400

    Attack animation bug fix

--- Enemy.py
+++ Enemy.py
@@ -18,8 +18,8 @@ class Enemy(Entity):
         self.current_sprite = 0
         self.scaling = 3
 
-        self.idle_image = self.attack_list[self.current_sprite]
-        self.attack_image = self.walk_list[self.current_sprite]
+        self.attack_image = self.attack_list[self.current_sprite]
+        self.walk_image = self.walk_list[self.current_sprite]
 
     def animate(self, image_list):
         self.current_sprite += 0.15
@@ -29,25 +29,25 @@ class Enemy(Entity):
     def update(self):
         if self.is_running_left:
             self.animate(self.walk_list)
-            self.attack_image = self.walk_list[int(self.current_sprite)]
+            self.walk_image = self.walk_list[int(self.current_sprite)]
             self.is_running_left = False
         if self.is_running_right:
             self.animate(self.walk_list)
-            self.attack_image = pygame.transform.flip(self.walk_list[int(self.current_sprite)], True, False)
+            self.walk_image = pygame.transform.flip(self.walk_list[int(self.current_sprite)], True, False)
             self.is_running_right = False
         if not self.is_running_right and not self.is_running_left and not self.is_facing_right:
             self.animate(self.attack_list)
-            self.idle_image = self.attack_list[int(self.current_sprite)]
+            self.attack_image = self.attack_list[int(self.current_sprite)]
         elif not self.is_running_right and not self.is_running_left and self.is_facing_right:
             self.animate(self.attack_list)
-            self.idle_image = pygame.transform.flip(self.attack_list[int(self.current_sprite)], True, False)
-            self.idle_image = self.attack_list[int(self.current_sprite)]
+            self.attack_image = pygame.transform.flip(self.attack_list[int(self.current_sprite)], True, False)
+            self.attack_image = self.attack_list[int(self.current_sprite)]
             self.is_facing_right = False
 
     def render_enemy(self, display, x, y):
         if self.is_running_right or self.is_running_left:
-            big_image = self.resize(self.attack_image, self.scaling)
+            big_image = self.resize(self.walk_image, self.scaling)
             display.blit(big_image, (self.start_x + x, self.start_y + y))
         elif not self.is_running_right or self.is_running_left:
-            big_idle = self.resize(self.idle_image, self.scaling)
+            big_idle = self.resize(self.attack_image, self.scaling)
             display.blit(big_idle, (self.start_x + x, self.start_y + y))
